19a5dc63f0603ff9571c6dbd72702272c1e48abc
十丿叶 <1372755472@qq.com>
2020-09-02 23:13:58 +0800

    update leetcode

--- Leetcode/leetcode/editor/cn/[1]两数之和.py
+++ Leetcode/leetcode/editor/cn/[1]两数之和.py
@@ -23,4 +23,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        for i, num in enumerate(nums):
+            if (target - num) in nums[i+1:]:
+                return [i, nums.index((target - num))]
 # leetcode submit region end(Prohibit modification and deletion)
