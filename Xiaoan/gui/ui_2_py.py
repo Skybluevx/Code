4c7924c824ba7b5f26e4f9fc4ed8794725f71a5c
小平板 <1372755472@qq.com>
2020-09-12 22:28:12 +0800

    update ui_2_py.py

--- Xiaoan/gui/ui_2_py.py
+++ Xiaoan/gui/ui_2_py.py
@@ -1,7 +1,15 @@
 import os
+import os.path
 
 
 def ui_2_py(ui_file_name):
+    file_names = os.listdir("./")
+
+    if ui_file_name not in file_names:
+        print("文件名错误或文件不存在，请重新检查！")
+        return
+
+    abs_ui_file_path = os.path.abspath(ui_file_name)
     py_file = ui_file_name.split(".")[0] + ".py"
     cmd = f"pyuic5 -o {py_file} {ui_file_name}"
     os.system(cmd)
