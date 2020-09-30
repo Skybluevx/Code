b90a6c9e8eabc07f91deb9e0302e0dc9a0043be5
小平板 <1372755472@qq.com>
2020-09-30 21:49:41 +0800

    sql的增删改查

--- Xiaoan/data/pysql_2.py
+++ Xiaoan/data/pysql_2.py
@@ -1,9 +1,17 @@
 import sqlite3
+from sqlite3.dbapi2 import Cursor
 
 conn = sqlite3.connect("./test.db")
 cur = conn.cursor()
-cur.execute("SELECT * FROM scores")
-# for i in data:
-#     print(i)
-print(cur.fetchall())
-print(type(cur.fetchall()))
+
+# cur.execute("CREATE TABLE IF NOT EXISTS test2(id INTEGER PRIMARY KEY ,name text, age integer)")
+# data = (1, "aqq", 20)
+# cur.execute(f"insert into test2 values {data}")
+# cur.execute("insert into test2 values (?,?,?)", (5, "leno", 22))
+cur.execute("select * from test2 where id=2")
+for i in cur:
+    print(i[0], i[1], i[2])
+
+conn.commit()
+cur.close()
+conn.close()
