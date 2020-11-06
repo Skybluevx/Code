390914687fe25ebc348a3e49e12266222923c665
小平板 <1372755472@qq.com>
2020-11-06 23:18:12 +0800

    修改投票页面

--- WebProject/DjangoProject/mysite/polls/views.py
+++ WebProject/DjangoProject/mysite/polls/views.py
@@ -1,16 +1,23 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Question, Choice
+
 
 # Create your views here.
 
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("你好呀，世界！")
+    latest_question_list = Question.objects.order_by("-pub_data")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context=context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"你正在看第{question_id}个问题")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/index.html", {"question": question})
 
 
 def result(request, question_id):
@@ -18,4 +25,15 @@ def result(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f"你正在给第{question_id}个问题投票")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "你没有选择一个选择",
+        })
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls/results", args=(question.id,)))
